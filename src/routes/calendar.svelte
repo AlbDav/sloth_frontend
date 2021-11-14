f216d896a7f43a9db2b8deb88f63b715d1654215
Davide Albano <da.albano@reply.it>
2021-11-14 20:38:44 +0100

    started installing fullcalendar

---
+++ src/routes/calendar.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { onMount } from 'svelte';
+	import FullCalendar from 'svelte-fullcalendar';
+
+	let options = { initialView: 'dayGridMonth', plugins: [] };
+
+	onMount(async () => {
+		options.plugins = [
+			(await import('@fullcalendar/daygrid')).default,
+		];
+	});
+</script>
+
+<FullCalendar {options} />
