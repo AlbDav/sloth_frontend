9563ee8f3f263cc03bcf9908e2514a7ad39ebde3
Davide Albano <davidealbano96@gmail.com>
2021-11-20 20:52:56 +0100

    started adding attractions

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,6 @@
 <script context="module">
 	import { browser, dev } from '$app/env';
+	import { Button } from 'attractions';
 
 	// we don't need any JS on this page, though we'll load
 	// it in dev so that we get hot module replacement...
@@ -39,6 +40,8 @@
 		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
 		it with JavaScript disabled!
 	</p>
+
+	<Button>ciao</Button>
 </div>
 
 <style>
--- src/routes/calendar.svelte
+++ src/routes/calendar.svelte
@@ -8,19 +8,44 @@
 	import dayGridPlugin from '@fullcalendar/daygrid';
 	import timeGridPlugin from '@fullcalendar/timegrid';
 	import listPlugin from '@fullcalendar/list';
+	import { addHours } from 'date-fns';
 
 	let calendarEl;
 	let calendar;
 
 	onMount(async () => {
+		await import('@fullcalendar/core/vdom');
 		calendar = new Calendar(calendarEl, {
 			plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
 			initialView: 'dayGridMonth',
 			headerToolbar: {
 				left: 'prev,next today',
 				center: 'title',
-				right: 'dayGridMonth,timeGridWeek,listWeek'
-			}
+				right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+			},
+			nowIndicator: true,
+			events: [
+				{
+					id: 'e1',
+					title: 'Prova',
+					start: Date.now(),
+					end: addHours(Date.now(), 1),
+					backgroundColor: 'red'
+				},
+				{
+					id: 'e2',
+					title: 'Prova 2',
+					start: addHours(Date.now(), -3),
+					end: addHours(Date.now(), -2),
+					backgroundColor: 'green'
+				},
+				{
+					id: 'e3',
+					title: 'Prova 3',
+					start: addHours(Date.now(), 2),
+					end: addHours(Date.now(), 3)
+				}
+			]
 		});
 		calendar.render();
 	});
